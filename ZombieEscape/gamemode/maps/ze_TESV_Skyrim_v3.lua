a2b8e2b843f4d66a1d8d7b048654459bd322c3ba
samuelmaddock <samuel.maddock@gmail.com>
2013-06-02 20:37:32 -0400

    Added skyrim map lua file

---
+++ ZombieEscape/gamemode/maps/ze_TESV_Skyrim_v3.lua
@@ -0,0 +1,13 @@
+--[[-------------------------------------------------------------------
+		Boss Entities
+---------------------------------------------------------------------]]
+
+-- Bosses seem to have their health broken up into segments for each
+-- part of their health bar. This doesn't work nicely with the sytem
+-- I've built.
+
+-- Stage 1 dragon
+--GM:AddBoss("Dragon", "alduin_1", "counter_1")
+
+-- Stage 2 dragon
+--GM:AddBoss("Dragon", "alduin_2", "counter_2")
