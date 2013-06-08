94ceca177d4d86966ec70a71a8f8094dbfb725ea
samuelmaddock <samuel.maddock@gmail.com>
2013-06-08 18:31:45 -0400

    Weapon dropping is now priority based

--- ZombieEscape/gamemode/modules/css/sv_weapondrop.lua
+++ ZombieEscape/gamemode/modules/css/sv_weapondrop.lua
@@ -1,21 +1,56 @@
+local Undroppable = {
+	weapon_crowbar = true
+}
+
 local function DropActiveWeapon( ply, cmd, args )
 
+	-- Only humans can drop weapons
 	if not ( IsValid(ply) and ply:IsHuman() ) then return end
 
+	-- Get out currently selected weapon
 	local weap = ply:GetActiveWeapon()
 	if not IsValid(weap) then return end
-	if weap:GetClass() == "weapon_crowbar" then return end
 
+	-- Ignore attempt to drop weapon if it's marked as undroppable
+	local class = weap:GetClass()
+	if Undroppable[class] then return end
+
+	-- Switch to weapon based on type
+	local weap2
+	if ply:GetPrimaryWeapon() == weap and ply:HasSecondaryWeapon() then
+		weap2 = ply:GetSecondaryWeapon()
+	elseif ply:GetSecondaryWeapon() == weap and ply:HasPrimaryWeapon() then
+		weap2 = ply:GetPrimaryWeapon()
+	else
+		if ply:HasPrimaryWeapon() then
+			weap2 = ply:GetPrimaryWeapon()
+		elseif ply:HasSecondaryWeapon() then
+			weap2 = ply:GetSecondaryWeapon()
+		else
+			for _, v in pairs( ply:GetWeapons() or {} ) do
+				if v:GetClass() != class then
+					weap2 = v
+					break
+				end
+			end
+		end
+	end
+
+	-- The player should always have a second valid weapon, but just in case..
+	if not IsValid(weap2) then return end
+
+	-- Select our other weapon
+	ply:SelectWeapon( weap2:GetClass() )
+
+	-- Drop the weapon we wanted to
 	ply:DropWeapon( weap )
 
-	weap = ply:GetActiveWeapon()
-	if not IsValid(weap) then return end
-	
 	-- Stupid fix for weapon not appearing after drop
-	if not weap:IsWeaponVisible() and weap:IsScripted() then
-		weap:RemoveEffects( EF_NODRAW )
-		weap:Deploy()
-	end
+	-- if not weap2:IsWeaponVisible() and weap2:IsScripted() then
+	-- 	weap2:RemoveEffects( EF_NODRAW )
+	-- 	weap2:Deploy()
+	-- end
+
 
 end
 concommand.Add( "ze_dropweapon", DropActiveWeapon )
--- ZombieEscape/gamemode/modules/weaponmenu/sv_meta.lua
+++ ZombieEscape/gamemode/modules/weaponmenu/sv_meta.lua
@@ -4,7 +4,8 @@ util.AddNetworkString("SelectWeapons")
 util.AddNetworkString("CloseWeaponSelection")
 
 function PlayerMeta:CanBuyWeapons()
-	return self:IsHuman() and ( ( !self.Weapons or !self.Weapons[1] or !self.Weapons[2] ) or
+	return self:IsHuman() and (
+		( !self.Weapons or !self.Weapons[1] or !self.Weapons[2] ) or
 		self.IsInBuyzone or !CVars.Buyzone:GetBool() )
 end
 
@@ -28,4 +29,50 @@ function PlayerMeta:GiveWeapons()
 		GAMEMODE:OnRequestWeapon(self, class, true)
 	end
 
+end
+
+function PlayerMeta:GetWeaponTier(tier)
+	for _, v in pairs( self:GetWeapons() or {} ) do
+		if self.Weapons[tier] == v:GetClass() then
+			return v
+		end
+	end
+end
+
+function PlayerMeta:SelectPrimaryWeapon()
+	local weapon = self:GetWeaponTier(WEAPON_PRIMARY)
+	if IsValid(weapon) then
+		self:SelectWeapon( weapon:GetClass() )
+	end
+end
+
+function PlayerMeta:SelectSecondaryWeapon()
+	local weapon = self:GetWeaponTier(WEAPON_SECONDARY)
+	if IsValid(weapon) then
+		self:SelectWeapon( weapon:GetClass() )
+	end
+end
+
+function PlayerMeta:GetPrimaryWeapon()
+	return self:GetWeaponTier(WEAPON_PRIMARY)
+end
+
+function PlayerMeta:GetSecondaryWeapon()
+	return self:GetWeaponTier(WEAPON_SECONDARY)
+end
+
+function PlayerMeta:HasWeaponTier(tier)
+	return IsValid( self:GetWeaponTier(tier) )
+end
+
+function PlayerMeta:HasPrimaryWeapon()
+	return self:HasWeaponTier(WEAPON_PRIMARY)
+end
+
+function PlayerMeta:HasSecondaryWeapon()
+	return self:HasWeaponTier(WEAPON_SECONDARY)
+end
+
+function PlayerMeta:HasAddonWeapon()
+	return self:HasWeaponTier(WEAPON_ADDON)
 end
