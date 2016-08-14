fd48b4d017ff9c873ec28827601ae53dcb504a3e
samuelmaddock <samuel.maddock@gmail.com>
2016-08-14 19:17:28 -0400

    Added ze_bioshock_v5_6 map file

---
+++ ZombieEscape/gamemode/maps/ze_bioshock_v5_6.lua
@@ -0,0 +1,6 @@
+--[[-------------------------------------------------------------------
+		Boss Entities
+---------------------------------------------------------------------]]
+
+-- Uses fragmented health bar which isn't currently supported
+-- GM:AddBoss("Big Daddy", "bouncer_mesh", "bouncer_Health_BarHP")
